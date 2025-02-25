43b030706b475d2bc60c844e019f30992f387ded
Sivishkin Maksim <MSivishkin127@gmail.com>
2025-02-25 11:05:17 +0300

    В методе main написан бесконечный цикл с проверкой корректности директории, наличия этой директории и присутствия файла в этой директории. При успешном исполнении выводит количество верно указанных файлов.

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,10 +1,26 @@
+import javax.sound.sampled.Line;
+import java.io.File;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
+        int att=0; // Накопитель количество верно указанных путей
+        while(true){ // Бесконечный цикл, запрашивает путь к файлу. Если путь не верный - цикл повторяется
+            System.out.println("Укажите путь к файлу: ");
+            String path = new Scanner(System.in).nextLine();
+            File file=new File(path);
+            boolean fileExists=file.exists();
+            boolean isDirectory=file.isDirectory();
+            boolean isFile=file.isFile();
+            if(fileExists&&isDirectory){
+                System.out.println("Путь указан верно");
+            }
+            else if(isFile){
+                System.out.println("Путь указан верно. Файл по данному пути присутствует");
+                att++;
+                System.out.println("Этот файл номер "+att);
+            }else System.out.println("Путь указан неверно");
+        }
 
-        System.out.println("Введите текст и нажмите <Enter>:");
-        String text = new Scanner(System.in).nextLine();
-        System.out.println("Длина текста " + text.length());
     }
 }
